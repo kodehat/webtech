890e8e4b338e5141a63d8f3bdd98f5520b4959b3
Pixelhash <dev@codehat.de>
2018-04-14 18:57:13 +0200

    Hasen-Textur hinzugefügt, Test der 3D-Lageerkennug-Implementierung

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,28 @@
 import 'dart:html';
 
+final HtmlElement startBtn = querySelector("#btn_start");
+HtmlElement rabbit = querySelector("#field_7_0");
+
+int row = 7;
+int col = 0;
+
+int betaOrientation = null;
+int betaToggleUp = null;
+int betaToggleDown = null;
+
+int gammaOrientation = null;
+int gammaToggleLeft = null;
+int gammaToggleRight = null;
+
+bool calibrated = false;
+bool hasMoved = false;
+
 void main() {
   generateGameField(8);
+
+  startBtn.onClick.listen(onStartBtnClick);
+
+  window.onDeviceOrientation.listen(onDeviceMove);
 }
 
 /// Generates the game field with the given size.
@@ -23,4 +44,46 @@ void generateGameField(int size) {
   }
 
   game.innerHtml = table;
+}
+
+void onStartBtnClick(MouseEvent e) {
+  print("Start button clicked!");
+  startBtn.remove();
+
+  rabbit.classes.toggle("rabbit");
+  calibrated = true;
+}
+
+void onDeviceMove(DeviceOrientationEvent e) {
+  if (e.alpha == null) return;
+
+  final int beta = e.beta.toInt();
+  final int gamma = e.gamma.toInt();
+
+  if (!calibrated) {
+    betaOrientation = beta;
+    betaToggleUp = betaOrientation - 20;
+    betaToggleDown = betaOrientation + 20;
+
+    gammaOrientation = gamma;
+    gammaToggleLeft = gammaOrientation - 20;
+    gammaToggleRight = gammaOrientation + 20;
+
+    return;
+  }
+
+  if (!hasMoved) {
+    if (beta <= betaToggleUp) { // Move UP
+     rabbit.classes.remove("rabbit");
+     row -= 1;
+     rabbit = querySelector("#field_${row}_${col}");
+     rabbit.classes.add("rabbit");
+
+     hasMoved = true;
+    }
+  } else {
+    if (beta >= betaToggleUp) {
+      hasMoved = false;
+    }
+  }
 }
