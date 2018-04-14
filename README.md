e3fd33b21c63a0e0735d541293e57f4d658012fe
Pixelhash <dev@codehat.de>
2018-04-14 13:31:32 +0200

    Beginn des Layouts

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # RabbitRun
 
 A maze game written in Dartlang.
+
+## Helpful sites
+
+- [How to Write Dart Comments](https://www.dartlang.org/guides/language/effective-dart/documentation#doc-comments)
+- [Flat UI Colors](https://flatuicolors.com)
+- [Nice Pixel Art Font](https://fonts.google.com/specimen/Press+Start+2P)
--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,26 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running. Yay!';
-  querySelector('#new').text = 'What the hell?';
+  generateGameField(8);
+}
+
+/// Generates the game field with the given size.
+///
+/// The [size] parameter defines the amount of
+/// columns and rows.
+void generateGameField(int size) {
+  var game = querySelector("#game");
+  String table = "";
+
+  for (int row = 0; row < size; row++) {
+    table += "<tr>";
+    for (int col = 0; col < size; col++) {
+      final pos = "field_${row}_${col}";
+
+      table += "<td id='$pos' class='field'>X</td>";
+    }
+    table += "</tr>";
+  }
+
+  game.innerHtml = table;
 }
